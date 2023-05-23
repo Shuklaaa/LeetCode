d88f9ca8892f614691dc5db1809c91af2c871627
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-05-23 14:21:40 +0530

    Time: 0 ms (100.00%), Space: 40 MB (13.44%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isThree(int n) {
-        int c = 0;
-        for(int i = 1; i <= n; i++){
+        int div = 2;   //1 and n are already divisors of n
+        for(int i = 2; i < n; i++){
             if(n % i == 0){
-                c++;
+                div++;
+            }
+            if(div > 3){
+                return false;
             }
         }
-        if(c == 3){
-            return true;
-        }
-        return false;
+        return div == 3;
     }
 }
