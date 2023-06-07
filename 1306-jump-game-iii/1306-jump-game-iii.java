05babbf2ab2183a4b139a69a8b88930a3bcbb283
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-06-07 17:28:13 +0530

    Time: 3 ms (98.94%), Space: 57.1 MB (23.21%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if(start < arr.length && start >= 0 && arr[start] >= 0){
+            if(arr[start] == 0){
+                return true;
+            }
+            
+            arr[start] = - arr[start];
+            
+            return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
+        }
+        return false;
+    }
+}
+
+
