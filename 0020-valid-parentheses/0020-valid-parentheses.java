d2699e0e56fca84ec5743f3b4a4a6bc6f8015feb
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-06-10 15:23:47 +0530

    Time: 3 ms (14.83%), Space: 40.5 MB (83.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,9 +2,10 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<Character>();
         
-        for (char it : s.toCharArray()){
-            if(it == '(' ||  it == '{' || it == '['){
-                st.push(it);
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                st.push(s.charAt(i));
             }
             else{
                 if(st.isEmpty()){ //it means for an opening bracket it does not have any closing bracket
@@ -12,7 +13,7 @@ class Solution {
                 }
                 
                 char c = st.pop();
-                if((it == ')' && c == '(') || (it == '}' && c == '{') || (it == ']' && c == '[')){
+                if((s.charAt(i) == ')' && c == '(') || (s.charAt(i) == '}' && c == '{') || (s.charAt(i) == ']' && c == '[')){
                     continue;
             }
                 else{
