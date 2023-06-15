1637508823b82d33561798f06f1d4911befbafd2
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-06-15 22:42:19 +0530

    Time: 11 ms (20.90%), Space: 39.1 MB (86.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -4,13 +4,6 @@ class Solution {
             return n;
         }
         
-        int prev = 0, prev1 = 1;
-        
-        for(int i = 2; i <= n; i++){
-            int curr = prev + prev1;
-            prev = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+        return fib(n - 1) + fib(n - 2);
     }
 }
