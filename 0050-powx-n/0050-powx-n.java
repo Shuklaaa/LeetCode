74b6ffc5fba424a1c3dbb3a8afb22dd245b72685
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-06-19 22:15:20 +0530

    Time: 1 ms (18.37%), Space: 41.4 MB (15.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,27 +1,26 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans = 1.0;
+        long num = n;
+        double ans = 1;
         
-        long nn = n;
-        
-        if(nn < 0){
-            nn = -1 * nn;
+        if(num < 0){
+            num = -num;
         }
         
-        
-        while(nn > 0){
-            if(nn % 2 == 1){
-                ans = ans * x;
-                nn = nn - 1;
-            }
-            else{
+        while(num > 0){
+            if(num % 2 == 0){
                 x = x * x;
-                nn /= 2;
+                num /= 2;
+            }
+            
+            else{
+                ans = ans * x;
+                num = num - 1;
             }
         }
         
         if(n < 0){
-            ans = (double)(1.0) / (double)(ans);
+            return 1.0/ans;
         }
         
         return ans;
