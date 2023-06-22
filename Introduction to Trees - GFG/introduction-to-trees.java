f0df4a2a42ceea7c70c4714cf20fcaec2e804890
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-06-22 20:57:57 +0530

    Added solution - LeetHub

--- Introduction to Trees - GFG/introduction-to-trees.java
+++ Introduction to Trees - GFG/introduction-to-trees.java
@@ -28,9 +28,7 @@ class GFG {
 class Solution {
     static int countNodes(int i) {
         // code here
-        if(i == 1){
-            return 1;
-        }
+
         return (int)(Math.pow(2, i - 1));
     }
 }
