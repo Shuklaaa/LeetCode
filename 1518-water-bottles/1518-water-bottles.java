43178695a3bdd81ba0151b25d34009b5fb9e640e
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-06-24 19:02:00 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (57.14%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int sum = numBottles;
+        
+        while(numBottles >= numExchange){
+            sum += numBottles / numExchange;
+            int temp = numBottles % numExchange;
+            
+            numBottles = numBottles / numExchange;
+            numBottles += temp;
+        }
+        
+        return sum;
+    }
+}
