c56c3a6ccdd0d858a03f9ed47b22919009430d0b
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-07-11 17:28:31 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (81.80%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,8 +1,8 @@
 class Solution {
     public int findComplement(int num) {
-        int bitL = (int)(Math.log(num) / Math.log(2));
+        int bitL = (int)(Math.log(num) / Math.log(2)) + 1;
         
-        bitL = bitL + 1;
+        // bitL = bitL + 1;
         
         int bitM = (1 << (bitL)) - 1;
         
