b6733c75b3be5cddc8d1970cd81093440cd2d55b
Abhishek Shukla <93446673+Shuklaaa@users.noreply.github.com>
2023-07-12 15:36:59 +0530

    Added solution - LeetHub

---
+++ Remove Duplicates from unsorted array - GFG/remove-duplicates-from-unsorted-array.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            int getAnswer[] = obj.removeDuplicate(a, n);
+            int sz = getAnswer.length;
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<sz;i++)
+                output.append(getAnswer[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    
+    public int[] removeDuplicate(int A[], int N)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        int[] result = new int[N];
+        int index = 0;
+        
+        for (int i = 0; i < N; i++) {
+            if (!set.contains(A[i])) {
+                set.add(A[i]);
+                result[index] = A[i];
+                index++;
+            }
+        }
+        
+        return Arrays.copyOf(result, index);
+    }
+}
